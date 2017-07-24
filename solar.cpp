7c9f8e5f4f5231f7920eb0f444e254d9f21c73f2
Daniel Campoverde <alx@sillybytes.net>
2017-07-23 23:51:23 -0500

    Faster sun spin

--- solar.cpp
+++ solar.cpp
@@ -494,7 +494,7 @@ Color colorUranus  = { 0.1, 0.8, 0.9 };
 Color colorNeptune = { 0.2, 0.3, 0.6 };
 Color colorMoon    = { 0.9, 0.9, 0.9 };
 
-AstronomicalObject sun     = {0.1, 0.05, 0, 0, 0, 0, 0, colorSun};
+AstronomicalObject sun     = {0.1, 0.1, 0, 0, 0, 0, 0, colorSun};
 AstronomicalObject mercury = {0.02, 0.1, 0.3, 0, 7.0, 0, 0, colorMercury};
 AstronomicalObject venus   = {0.025, 0.1, 0.25, 0, 8.0, 0, 0, colorVenus};
 AstronomicalObject earth   = {0.03, 0.1, 0.15, 0, 10.3, 0, 0, colorEarth};
